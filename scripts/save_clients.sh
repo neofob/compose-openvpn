2cc2d90d152612c0f9029ff976e5f49c9e5203f0
Tuan T. Pham <tuan@vt.edu>
2024-08-11 18:34:49 -0400

    Update config for pihole combo
    default_settings.env:
        * Set static IP for pihole and openvpn container
    
    docker-compose-pi.yml:
        * Use env var for static IP
    
    scripts/save_clients.sh:
        * Use dos2unix to sanitize output config files
    
    Signed-off-by: Tuan T. Pham <tuan@vt.edu>

--- scripts/save_clients.sh
+++ scripts/save_clients.sh
@@ -3,4 +3,6 @@
 for client in $(make list | tail  +8 | cut -d "," -f1); do
     echo "Saving config file for $client"
     make OVPN_CLIENT=$client get_client
+    echo "script-security 2" >> /tmp/$client.ovpn
+    dos2unix /tmp/$client.ovpn
 done
