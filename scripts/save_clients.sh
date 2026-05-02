10893e17f1ab687ef659130e33c6102c701099bd
Tuan T. Pham <tuan@vt.edu>
2026-05-02 08:39:53 -0400

    Fix up bad parameters
    Signed-off-by: Tuan T. Pham <tuan@vt.edu>

--- scripts/save_clients.sh
+++ scripts/save_clients.sh
@@ -5,18 +5,9 @@ for client in $(make list | grep "VALID$" | cut -d "," -f1); do
     make OVPN_CLIENT=$client get_client
     echo "script-security 2" >> /tmp/$client.ovpn
 
-	# Fine-tune
-# mssfix 0
-# fast-io
-# sndbuf 524288
-# rcvbuf 524288
-# txqueuelen 2000
-
 	cat <<EOF >> /tmp/$client.ovpn
-mssfix 0
+mssfix 1280
 fast-io
-sndbuf 524288
-rcvbuf 524288
 txqueuelen 2000
 EOF
 
